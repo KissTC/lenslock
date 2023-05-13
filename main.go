2636a0d437116a52d5bb2b0cf97496575484da22
Julio Paredes <ing.julio.code@gmail.com>
2023-05-13 14:52:33 -0600

    creating the views package and the template struct to parse and execute

--- main.go
+++ main.go
@@ -2,32 +2,27 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
+
+	"github.com/kisstc/lenslock/views"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// parsing the template and executed it
-	// using path
-	tpl, err := template.ParseFiles(filepath)
+
+	t, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
 
-	err = tpl.Execute(w, "a string")
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
+
 }
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
